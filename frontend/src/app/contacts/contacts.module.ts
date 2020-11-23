391c6f6135ab793e8485a868e02a544879d345e1
Daniele Facco <d.facco.78@gmail.com>
2020-11-23 15:21:00 +0100

    create store

--- frontend/src/app/contacts/contacts.module.ts
+++ frontend/src/app/contacts/contacts.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactsComponent } from './contacts.component';
+import { StoreModule } from '@ngrx/store';
+import * as fromContacts from './reducers';
 
 
 
 @NgModule({
   declarations: [ContactsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forFeature(fromContacts.contactsFeatureKey, fromContacts.reducers)
   ],
   exports: [
     ContactsComponent
