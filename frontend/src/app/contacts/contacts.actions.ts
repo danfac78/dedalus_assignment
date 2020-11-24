38a403cb66cdc810b0f6ca3390698b5549fd698b
Daniele Facco <d.facco.78@gmail.com>
2020-11-24 23:36:42 +0100

    Add an action to load data

--- frontend/src/app/contacts/contacts.actions.ts
+++ frontend/src/app/contacts/contacts.actions.ts
@@ -1,11 +1,18 @@
-import { createAction } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
+import { Contacts } from './core/contact.model';
 
 
 const PREFIX = '[Contacts Container]';
 enum Actions {
-    load = 'load'
+    load = 'load',
+    loaded = 'loaded'
 }
 
 export const loadContacts = createAction(
     `${PREFIX} ${Actions.load}`
+);
+
+export const loadedContacts = createAction(
+    `${PREFIX} ${Actions.loaded}`,
+    props<{ contacts: Contacts }>()
 );
