390f92a6ca41489499a0bd980172cdbf647d9314
Daniele Facco <d.facco.78@gmail.com>
2020-11-28 13:24:26 +0100

    Made unit tests more readable

--- frontend/src/app/contacts/list/list.component.spec.ts
+++ frontend/src/app/contacts/list/list.component.spec.ts
@@ -42,28 +42,30 @@ describe('ListComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('setting input value and clicking on a "td" element', () => {
+  const CONTACTS_DATA: Contact[] = [
+    new Contact({
+      firstname: 'john',
+      lastname: 'johnson',
+      telephone: '1234567890',
+      address: 'St. John Street, 12, Johnstown, Pennsylvania',
+      email: 'john.johnson@gmail.com'
+    }),
+    new Contact({
+      firstname: 'peter',
+      lastname: 'peterson',
+      telephone: '2345678901',
+      address: 'Peter Avenue Park 13, Johannesburg, Sudafrica',
+      email: 'p.peterson@gmail.com'
+    })
+  ];
+
+  describe('setting input value and clicking on a table data element', () => {
     it('should store a Contact', () => {
       // arrange
       component.ngOnInit();
-      component.contacts = [
-        new Contact({
-          firstname: 'john',
-          lastname: 'johnson',
-          telephone: '1234567890',
-          address: 'St. John Street, 12, Johnstown, Pennsylvania',
-          email: 'john.johnson@gmail.com'
-        }),
-        new Contact({
-          firstname: 'peter',
-          lastname: 'peterson',
-          telephone: '2345678901',
-          address: 'Peter Avenue Park 13, Johannesburg, Sudafrica',
-          email: 'p.peterson@gmail.com'
-        })
-      ];
+      component.contacts = CONTACTS_DATA;
 
-      const EXPECTED_CONTACT = component.contacts[1];
+      const EXPECTED_CONTACT = CONTACTS_DATA[1];
 
       fixture.detectChanges();
 
@@ -85,73 +87,61 @@ describe('ListComponent', () => {
   });
 
   describe('setting input value and clicking on an table header', () => {
-    it('should store a Contact', () => {
+    it('should not change the contact in the store', () => {
       // arrange
       component.ngOnInit();
-      component.contacts = [
-        new Contact({
-          firstname: 'john',
-          lastname: 'johnson',
-          telephone: '1234567890',
-          address: 'St. John Street, 12, Johnstown, Pennsylvania',
-          email: 'john.johnson@gmail.com'
-        }),
-        new Contact({
-          firstname: 'peter',
-          lastname: 'peterson',
-          telephone: '2345678901',
-          address: 'Peter Avenue Park 13, Johannesburg, Sudafrica',
-          email: 'p.peterson@gmail.com'
-        })
-      ];
+      component.contacts = CONTACTS_DATA;
+
+      const EXPECTED_CONTACT = CONTACTS_DATA[1];
 
       fixture.detectChanges();
 
       // act
       const store: Store<AppState> = TestBed.inject(Store);
       const contactsTableFlat = fixture.debugElement.queryAllNodes(By.all());
-      const clickedTableRows = contactsTableFlat.map(n => n.nativeNode).filter(n => n.localName === 'tr');
+      const clickedTableRows: HTMLTableDataCellElement[] = contactsTableFlat.map(n => n.nativeNode).filter(n => n.localName === 'tr');
       clickedTableRows[0].click();
+
+      const clickedTable: HTMLTableElement = contactsTableFlat.map(n => n.nativeNode).filter(n => n.localName === 'table').pop();
+      clickedTable.click();
 
       // assert
       store.subscribe(c => {
-        expect((c as SELECTED_CONTACT_SHORTCUT).contacts.selectedContact).toBeNull();
+        expect((c as SELECTED_CONTACT_SHORTCUT).contacts.selectedContact.firstname).toEqual(EXPECTED_CONTACT.firstname);
+        expect((c as SELECTED_CONTACT_SHORTCUT).contacts.selectedContact.lastname).toEqual(EXPECTED_CONTACT.lastname);
+        expect((c as SELECTED_CONTACT_SHORTCUT).contacts.selectedContact.telephone).toEqual(EXPECTED_CONTACT.telephone);
+        expect((c as SELECTED_CONTACT_SHORTCUT).contacts.selectedContact.address).toEqual(EXPECTED_CONTACT.address);
+        expect((c as SELECTED_CONTACT_SHORTCUT).contacts.selectedContact.email).toEqual(EXPECTED_CONTACT.email);
       });
     });
   });
 
   describe('setting input value and clicking on the table element', () => {
-    it('should store a Contact', () => {
+    it('should not change the contact in the store', () => {
       // arrange
       component.ngOnInit();
-      component.contacts = [
-        new Contact({
-          firstname: 'john',
-          lastname: 'johnson',
-          telephone: '1234567890',
-          address: 'St. John Street, 12, Johnstown, Pennsylvania',
-          email: 'john.johnson@gmail.com'
-        }),
-        new Contact({
-          firstname: 'peter',
-          lastname: 'peterson',
-          telephone: '2345678901',
-          address: 'Peter Avenue Park 13, Johannesburg, Sudafrica',
-          email: 'p.peterson@gmail.com'
-        })
-      ];
+      component.contacts = CONTACTS_DATA;
+
+      const EXPECTED_CONTACT = CONTACTS_DATA[1];
 
       fixture.detectChanges();
 
       // act
       const store: Store<AppState> = TestBed.inject(Store);
       const contactsTableFlat = fixture.debugElement.queryAllNodes(By.all());
-      const clickedTableRows = contactsTableFlat.map(n => n.nativeNode).filter(n => n.localName === 'table');
-      clickedTableRows[0].click();
+      const clickedTableRows: HTMLTableDataCellElement[] = contactsTableFlat.map(n => n.nativeNode).filter(n => n.localName === 'td');
+      clickedTableRows[2].click();
+
+      const clickedTable: HTMLTableElement = contactsTableFlat.map(n => n.nativeNode).filter(n => n.localName === 'table').pop();
+      clickedTable.click();
 
       // assert
       store.subscribe(c => {
-        expect((c as SELECTED_CONTACT_SHORTCUT).contacts.selectedContact).toBeNull();
+        expect((c as SELECTED_CONTACT_SHORTCUT).contacts.selectedContact.firstname).toEqual(EXPECTED_CONTACT.firstname);
+        expect((c as SELECTED_CONTACT_SHORTCUT).contacts.selectedContact.lastname).toEqual(EXPECTED_CONTACT.lastname);
+        expect((c as SELECTED_CONTACT_SHORTCUT).contacts.selectedContact.telephone).toEqual(EXPECTED_CONTACT.telephone);
+        expect((c as SELECTED_CONTACT_SHORTCUT).contacts.selectedContact.address).toEqual(EXPECTED_CONTACT.address);
+        expect((c as SELECTED_CONTACT_SHORTCUT).contacts.selectedContact.email).toEqual(EXPECTED_CONTACT.email);
       });
     });
   });
